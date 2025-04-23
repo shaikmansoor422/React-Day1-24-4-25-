3a2008635f8883280002442f4b36373579d7af7b
Mansoor shaik <125723822+shaikmansoor422@users.noreply.github.com>
2025-04-24 01:25:38 +0530

    First_commit

---
+++ Navbar/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+
+function Navbar() {
+  return (
+    <>
+      <div id="bg">
+        <div id="nav_container">
+          <div id="heading">React</div>
+          <div id="details">
+            <span >History</span>
+            <span>Documents</span>
+            <span>Install</span>
+            <span>Help</span>
+          </div>
+        </div>
+        <p id="msg">Welcome to React.JS</p>
+      </div>
+
+    </>
+  )
+}
+export default Navbar
